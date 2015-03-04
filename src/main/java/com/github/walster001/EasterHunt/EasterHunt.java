be81480035d94ef0180999358d7e4a093b7b92ea
BuildTools <unconfigured@null.spigotmc.org>
2015-03-04 17:52:14 +1030

    Initial Commit

---
+++ src/main/java/com/github/walster001/EasterHunt/EasterHunt.java
@@ -0,0 +1,36 @@
+package com.github.walster001.EasterHunt;
+ 
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public final class EasterHunt extends JavaPlugin {
+	@Override
+    public void onEnable() {
+		getLogger().info("EasterHunt by Walster001 Loaded Successfully!");
+		
+    }
+ 
+    @Override
+    public void onDisable() {
+    	getLogger().info("EasterHunt by Walster001 Disabled Successfully!");
+    	// TODO Insert logic to be performed when the plugin is disabled
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String eh, String[] args) {
+    	if (cmd.getName().equalsIgnoreCase("easterhunt")) { // If the player typed /basic then do the following...
+    		// doSomething
+    		return true;
+    	} else if (cmd.getName().equalsIgnoreCase("easterhunt start")) {
+    		if (!(sender instanceof Player)) {
+    			sender.sendMessage("This command cannot be run from the console.");
+    		} else {
+    		}
+    		//doSomethingViaPlayer
+    		return true;
+    	} //If this has happened the function will return true. 
+            // If this hasn't happened the value of false will be returned.
+    	return false; 
+    }
+}
